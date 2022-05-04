cc2c55b4797b0eb947a160e5a3e618b9f3ca7309
Kim BJ <100742437+Kim-qjaJ@users.noreply.github.com>
2022-05-04 13:29:27 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptolls
+
+setuptools.setup (
+  include_package_data = True,
+  name='myossprc',
+  cersion='0.0.1',
+  description='oss-dev my oss practice',
+  author='Kim-qjaJ',
+  author_email='neom9245@gmail.com',
+  url = "https://github.com/Kim-qjaJ/ospsypi0502",
+  download_url = "https://github.com/Kim-qjaJ/ospsypi0502/archive/refs/tags/v0.0.1.zip",
+  install_requires=['pytest'],
+  long_description = 'oss-dev practice module',
+  long_description_content_type = 'text/markdown',
+  classifiers[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
